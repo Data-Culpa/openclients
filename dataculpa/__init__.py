6f596799b48b3778afc050e24262470b4b9d686a
Mitch Haile <mitch@dataculpa.com>
2020-05-15 16:58:54 -0400

    Some API changes... MIT license

--- dataculpa/__init__.py
+++ dataculpa/__init__.py
@@ -2,20 +2,46 @@
 # __init__.py
 # Data Culpa Python Client
 #
-# Copyright © 2020 Data Culpa, Inc. All rights reserved.
+# Copyright (c) 2020 Data Culpa, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
 #
 
 import json
 import requests
 
-class DataCulpaHttpRest:
+class DataCulpaValidator:
+    HTTP = "http"
 
-    def __init__(self, dc_host, dc_port=7777):
+    def __init__(self, protocol, dc_host, dc_port=7777):
+        self.protocol = protocol
+        assert self.protocol == self.HTTP, "invalid protocol: only http is supported in this release"
+
         self.host = dc_host
         self.port = dc_port
+
         # HTTPS vs HTTP
         # HTTP proxies
         # Lots of things to add here.
+
+        # FIXME: secrets handling.
+
 
     def test_connection(self):
         # FIXME: need to do something useful.
@@ -25,7 +51,12 @@ class DataCulpaHttpRest:
     def _get_base_url(self):
         return "http://%s:%s/" % (self.host, self.port)
 
-    def validate_blocking(self, pipeline_name, record_set):
+    def validate_blocking(self, 
+                          record_set,
+                          pipeline_name, 
+                          pipeline_stage="default",
+                          pipeline_environment="default",
+                          metadata=None):
 
         rs_str = json.dumps(record_set)
 
@@ -34,10 +65,28 @@ class DataCulpaHttpRest:
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         r = requests.post(url=self._get_base_url() + path, data=rs_str, headers=headers)
         print("RESPONSE: ", r)
+        print(r.content)
         return
     
-    def validate_async(self, pipeline_name, record_set):
-        # call and immediately return, not waiting for server connection.
+    def validate_async(self,
+                       pipeline_name, 
+                       pipeline_stage,
+                       pipeline_environment,
+                       record_set,
+                       extra_metadata):
+        # call and immediately return, nhttpot waiting for server connection.
         # FIXME: timeouts on network connectivity, etc.
+        job_id = "not implemented"
+        return job_id
+
+    def validate_update(self,
+                        job_id,
+                        additional_metadata=None,
+                        is_finished=False):
+        "not implemented"
         return
 
+    def validate_complete(self, job_id):
+        """This is the same as calling validate_update(job_id, is_finished=True)"""
+        return self.validate_update(job_id, is_finished=True)
+    
--- test/basic.py
+++ test/basic.py
@@ -1,6 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# basic.py
+# Data Culpa Open Clients
+#
+# Copyright (c) 2020 Data Culpa, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+#
 
-
-from dataculpa import DataCulpaHttpRest
+from dataculpa import DataCulpaValidator
 from datetime import datetime
 
 import random
@@ -10,7 +35,9 @@ import json
 
 def main():
 
-    dc = DataCulpaHttpRest("localhost", 7777)
+    dc = DataCulpaValidator(DataCulpaValidator.HTTP,
+                            "192.168.1.13", 
+                            7777)
 
     # dc.test_connection()
 
@@ -19,7 +46,7 @@ def main():
           'run_time': str(datetime.now()),
           'random_value': random.random() }
 
-    dc.validate_blocking("test-pipeline", [d])
+    dc.validate_blocking([d], "test-pipeline")
     return
 
 if __name__ == "__main__":
