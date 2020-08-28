5db2ac7fb97c181523f6e16b78e18ef4bb832167
Mitch Haile <mitch@dataculpa.com>
2020-08-28 12:10:47 -0400

    A number of changes...

--- dataculpa/validator.py
+++ dataculpa/validator.py
@@ -33,12 +33,24 @@ try:
 except:
     pass 
 
+
+def show_versions():
+    # print out some version debug stuff... 
+    # see sklearn.show_versions() for inspiration
+    print("Need to implement show_version!")
+    return    
+
 class DataCulpaValidator:
     HTTP = "http"
     HTTPS = "https"
     # FIXME: http, message queues, etc.
 
-    def __init__(self, protocol, dc_host, dc_port=7777):
+    def __init__(self, 
+                 protocol, 
+                 dc_host, 
+                 dc_port=7777, 
+                 api_access_id=None, 
+                 api_secret=None):
         self.protocol = protocol
         #if self.protocol == self.HTTP:
         #    assert dc_host == "localhost", "HTTP is only supported for localhost"
@@ -47,49 +59,31 @@ class DataCulpaValidator:
 
         self.host = dc_host
         self.port = dc_port
+        self.api_access_id = api_access_id
+        self.api_secret = api_secret
 
     def test_connection(self):
-        # FIXME: need to do something useful.
-            # maybe also verify we can login, etc.
-        raise Exception("Not implemented")
+        url = self._get_base_url() + "test/connection"
+        r = requests.get(url=url,
+                         headers=self._json_headers())
+        if r.status_code != 200:
+            print("got status code %s for %s" % (r.status_code, url))
+            return 1
+
+        try:
+            jr = json.loads(r.content)
+            if jr.get('status') is None:
+                print("missing status")
+                return 1
+        except:
+            print("Error parsing result: __%s__", r.content)
+            return 1
+
+        # FIXME: needs more error handling.
+        return 0
 
     def _get_base_url(self):
         return "%s://%s:%s/" % (self.protocol, self.host, self.port)
-
-    def validate_blocking(self, 
-                          record_set,
-                          pipeline_name, 
-                          pipeline_environment="default",
-                          pipeline_stage="default",
-                          pipeline_version="default",
-                          metadata=None):
-
-        if HAS_PANDAS:
-            # FIXME: check for record_set being a pandas dataframe, etc.
-            pass
-
-        rs_str = json.dumps(record_set)
-
-        # call the validator server, wait for feedback
-        path = "validate/" + pipeline_name
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.post(url=self._get_base_url() + path, 
-                          data=rs_str, 
-                          headers=headers)
-        return r.content
-    
-    def validate_async(self,
-                       record_set,
-                       pipeline_name, 
-                       pipeline_environment="default",
-                       pipeline_stage="default",
-                       extra_metadata=None):
-        # call and immediately return, nhttpot waiting for server connection.
-        # FIXME: timeouts on network connectivity, etc.
-
-
-        job_id = "not implemented"
-        return job_id
 
     def _build_pipeline_url_suffix(self,
                                    pipeline_name, 
@@ -134,13 +128,10 @@ class DataCulpaValidator:
             print("Error parsing result: __%s__", r.content)
         return (None, 0, 0)
 
-    def queue_interim_validate(self, queue_id):
-        path = "queue/interim_validate/%s" % queue_id
-        return
-
     def queue_commit(self, queue_id):
         path = "queue/commit/%s" % queue_id
-        r = requests.post(url=self._get_base_url() + path, 
+        url = self._get_base_url() + path
+        r = requests.post(url=url, 
                           data="", 
                           headers=self._json_headers())
         try:
@@ -150,15 +141,16 @@ class DataCulpaValidator:
             print("Error parsing result: __%s__", r.content)
         return None
 
-    def validate_update(self,
-                        job_id,
-                        additional_metadata=None,
-                        is_finished=False):
-        # what do we do with this additional metadata?
-        "not implemented"
-        return
+    def validation_status(self, queue_id):
+        path = "validation/status/%s" % queue_id
+        url = self._get_base_url() + path
+        r = requests.get(url=url, headers=self._json_headers())
+        if r.status_code != 200:
+            return "Error"
 
-    def validate_complete(self, job_id):
-        """This is the same as calling validate_update(job_id, is_finished=True)"""
-        return self.validate_update(job_id, is_finished=True)
-    
+        try:
+            jr = json.loads(r.content)
+            return jr
+        except:
+            print("Error parsing result: __%s__", r.content)
+        return None
