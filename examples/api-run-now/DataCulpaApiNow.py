83aa57e68199f42f3662dda2b3dae0cff87558d5
mitch haile <mitch.haile@gmail.com>
2022-04-06 08:39:10 -0400

    Some additional docs

--- examples/api-run-now/DataCulpaApiNow.py
+++ examples/api-run-now/DataCulpaApiNow.py
@@ -38,20 +38,30 @@ from dataculpa import DataCulpaValidator, DataCulpaWatchpointNotDefined, DataCul
 DC_USER = os.environ['DC_API_USER']
 DC_SECRET = os.environ['DC_API_SECRET']
 
-DC_HOST = "demo-beagle-456.dataculpa.cloud"
+DC_HOST = os.environ['DC_HOST'] # domain only; no https:// prefix or port
 DC_PORT = 7778
 
 def main():
     # Connect to Validator
-    dc = DataCulpaValidator("watchpoint-name",
+    dc = DataCulpaValidator(watchpoint_name=None, #"watchpoint-name",
                             # Can also specify other watchpoint metadata here to specify a more specific watchpoint.
-                            # This will create a new watchpoint if it does not exist.
+                            # Specifying a name here will create a new watchpoint if it does not exist;
+                            # pass None for the watchpoint_name and use setWatchpointName() to specify a name
+                            # without creating the watchpoint.
                             protocol=DataCulpaValidator.HTTPS,
                             dc_host=DC_HOST,
                             dc_port=DC_PORT,
                             api_access_id=DC_USER,
                             api_secret=DC_SECRET)
     
+    res = dc.getWatchpointVariations("watchpoint-name2")
+    # res = [{'context': 'default',
+    #         'create_time': '2022-04-06T04:29:52.075885',
+    #         'name': 'watchpoint-name',
+    #         'stage': 'default',
+    #         'version': 'default'}]
+    # dc.setWatchpointName(res[0].get('name'))
+
     # Some undercover verification:
     # watchpoint_id = dc._get_pipeline_id()
     # dc._pipeline_id = XX      # Can set or check this to ensure the id matches the URL in the Validator UI
